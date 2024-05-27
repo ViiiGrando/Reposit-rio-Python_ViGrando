8f8f669c0450a8492b6be346cb0cb2464962fc56
ViiiGrando <162319269+ViiiGrando@users.noreply.github.com>
2024-05-27 18:49:16 -0300

    Add files via upload

---
+++ parametrospadrao.py
@@ -0,0 +1,8 @@
+def imprimir_nome(nome=None, sobrenome=None):
+   if nome and sobrenome != None: 
+      print('nome:', nome)
+   else:
+      print('Digite seu nome')
+
+
+imprimir_nome('Victor', 'Grando')     
