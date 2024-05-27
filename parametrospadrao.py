9d8cec5b393098f527424ac6aec4247689f22baa
ViiiGrando <162319269+ViiiGrando@users.noreply.github.com>
2024-05-27 19:08:30 -0300

    Add files via upload

--- parametrospadrao.py
+++ parametrospadrao.py
@@ -1,8 +1,17 @@
-def imprimir_nome(nome=None, sobrenome=None):
-   if nome and sobrenome != None: 
-      print('nome:', nome)
-   else:
-      print('Digite seu nome')
+'''def imprimir_nome(nome=str, sobrenome=str):
+
+  pass
 
 
-imprimir_nome('Victor', 'Grando')     
+
+imprimir_nome('Victor', '95')    
+'''
+
+
+#KWARDS
+def imprimir_nome(**nomes):
+    print(f'{nomes['nome']} {nomes['sobrenome']}')
+
+imprimir_nome(nome = 'VI', sobrenome = 'GRA')
+
+#KWARDS São basicamente um dicionário para um parametro, quando a gente não sabe quantos argumentos serão utilizados em uma função
